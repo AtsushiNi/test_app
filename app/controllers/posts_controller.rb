7e91278baf4d918badf393e727f8669f867369ab
Atsushi <atsushi9731@gmail.com>
2018-12-27 11:10:46 +0900

    finish static pages

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
