d71f4788f6fc10cd82ab53aa8a101028961b4c8f
Atsushi <atsushi9731@gmail.com>
2018-12-27 12:51:05 +0900

    finish

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :only_logged_in, only: [:new, :edit, :create, :update, :destroy]
+  before_action :only_current_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,20 +11,48 @@ class PostsController < ApplicationController
   end
 
   def new
-  end
-
-  def edit
+    @post = Post.new
   end
 
   def edit
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.email = current_user.email
+    @post.save
+    redirect_to @post
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def only_logged_in
+    if !user_signed_in?
+      redirect_to root_url
+    end
+  end
+
+  def only_current_user
+    @post = Post.find(params[:id])
+    if current_user.email != @post.email
+      flash[:alert] = "Not yours"
+      redirect_to posts_url
+    end
+  end
+
 end
